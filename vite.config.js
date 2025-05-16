f35f7078ed34de02209b96a95196bad3a9841286
Mikael Mota <mikael.mota@localize.com.br>
2025-05-15 23:36:01 -0300

    build: fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 export default defineConfig(({ mode }) => {    
     const isProduction = mode === 'production';
     return {
+        build: {
+            target: "es2022"
+        },
         base: isProduction ? '/grimorio-tormenta-20/' : '/',
         plugins: [
             vue({
